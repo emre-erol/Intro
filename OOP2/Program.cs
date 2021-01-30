6e7a4cb3984dd83a76af00347bdfd387f6705ab3
emre-erol <erol.emre@outlook.com>
2021-01-30 18:41:28 +0300

    23 ocak

---
+++ OOP2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Emre";
+            musteri1.Soyadi = "Erol";
+            musteri1.TcNo = "12354467890";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "12346";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            //Gerçek müşter--Tüzel müşteri
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+         
+        }
+    }
+}
---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person();
+            person1.FirstName = "Emre";
+            person2 = person1;
+            person1.FirstName = "İrem";
+            //Console.WriteLine(person2.FirstName);
+
+            Customer customer = new Customer();
+            customer.FirstName = "Mert";
+            customer.CreditCardNumber = "1234567890"    ;
+
+            Employee employee = new Employee();
+            employee.FirstName = "Kağan";
+            Person person3 = customer;
+            customer.FirstName = "Derin";
+
+            //Console.WriteLine(((Customer)person3).CreditCardNumber);
+
+            PersonerManager personerManager = new PersonerManager();
+            personerManager.Add(employee);
+
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer:Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
+    class Employee:Person
+    {
+        public int EmployeeNumber { get; set; }
+    }
+
+    class PersonerManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
